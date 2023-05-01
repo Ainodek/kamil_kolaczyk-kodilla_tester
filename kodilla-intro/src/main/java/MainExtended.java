590298c9733c204622a7e0b397028df01555cd0c
Kamil Kolaczyk <ainodek@gmail.com>
2023-05-01 20:38:02 +0200

    MainExtedned

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,13 @@
+public class MainExtended {
+    public static void main(String[] args) {
+
+        String myName = "Kamil";
+        double height = 1.82;
+        int age = 30;
+        boolean male = true;
+        char firstLetterSurname = 'K';
+        String hobby = "akwarystyka";
+
+        System.out.println("Na imię mam " + myName + ", pierwsza litera mojego nazwiska to " + firstLetterSurname + ", mam " + age + " lat, mierzę " + height + ", a moim hobby jest " + hobby + ". Czy jestem facetem? " + male);
+    }
+}
