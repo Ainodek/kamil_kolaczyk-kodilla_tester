52be38933933be0eca14dd17fe9e92123d1c2faa
Kamil Kolaczyk <ainodek@gmail.com>
2023-06-04 20:20:55 +0200

    Loop

--- kodilla-intro/src/main/java/Loop.java
+++ kodilla-intro/src/main/java/Loop.java
@@ -1,5 +1,8 @@
+import java.util.Random;
+
 public class Loop {
     public int suma(int[] numbers) {
+
         int numbersOfElements = numbers.length;
         int sumOfArray = 0;
 
@@ -11,4 +14,17 @@ public class Loop {
 
         return sumOfArray;
     }
+/*
+    public int getCountOfRandomNumber(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(10);
+            sum = sum + temp;
+            result++;
+        }
+        return result;
+    }
+*/
 }
